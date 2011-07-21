c658ebdb7c94974b5e010471c376280bd4a1e8fa
thienbao <thienbao@ubuntu.(none)>
2011-07-22 00:41:26 +0900

    Firt Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '2805f590e3f010873c79f1df89c6cbda5ad14181bb5b7356b98313a0df3dad16348ea8e4efc9e508dabedf2f8bfd8353f2b3d4a0d71c8e4735ea030fc0b83a9c'
